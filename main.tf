6b52f814969af206225721e531131b1958ec2aff
georgeisacnewton <georgeisacnewton@gmail.com>
2019-05-06 18:07:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "nsg" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
+  depends_on          = ["module.vnet"]
   name                = "${var.security_group_name}"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.nsg.name}"
